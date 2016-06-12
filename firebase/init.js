779918bda62eff4f1fc967295099cbb988487d88
JoonChul Kim <jeta@jetalog.net>
2016-06-12 16:23:27 +0900

    Setting .gitignore

--- firebase/init.js
+++
@@ -1,8 +0,0 @@
-// Initialize Firebase
-var config = {
-    apiKey: "API_KEY",
-    authDomain: "AUTH_DOMAIN",
-    databaseURL: "DATABASE_URL",
-    storageBucket: "STORAGE_BUCKET",
-};
-firebase.initializeApp(config);

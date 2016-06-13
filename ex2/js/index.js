0174aa4549d635ab5f08dd7af823e1cf70425c5c
JoonChul Kim <jeta@jetalog.net>
2016-06-13 23:04:20 +0900

    Update Ex2
    Added Sign In With Redirect

--- ex2/js/index.js
+++ ex2/js/index.js
@@ -1,64 +1,65 @@
 $(document).ready(function() {
     $("#authorized").hide();
     $("#error").hide();
+    
+    firebase.auth().getRedirectResult()
+        .then(signInSucceed)
+        .catch(signInError);
 });
 
 function signIn() {
     var provider = new firebase.auth.GoogleAuthProvider();
     
     firebase.auth().signInWithPopup(provider)
-            .then(function(result) {
-                googleAccountToken = result.credential.accessToken;
-                user = result.user;
-        
-                $("#photo").attr("src", user.photoURL);
-                $("#displayName").html(user.displayName);
-                $("#email").html(user.email);
-                $("#refreshToken").html(user.refreshToken);
-                $("#uid").html(user.uid);
-        
-                $("#authorized").show();
-                $("#signIn").hide();
-            })
-            .catch(function(error) {
-                var errorCode = error.code;
-                var errorMessage = error.message;
-                var email = error.email;
-                var credential = error.credential;
-
-                var errmsg = errorCode + " " + errorMessage;
-        
-                if(typeof(email) != 'undefined') {
-                    errmsg += "<br />";
-                    errmsg += "Cannot sign in with your google account: " + email;
-                }
-        
-                if(typeof(credential) != 'undefined') {
-                    errmsg += "<br />";
-                    errmsg += credential;
-                }
-        
-                lastWork = "signIn";
-                $("#error #errmsg").html(errmsg);
-                $("#error").show();
-                $("#signIn").hide();
-                return;
-            });
+            .then(signInSucceed)
+            .catch(signInError);
 }
 
-function signOut() {
-    if(!confirm("Do you really want to log out?")) {
-        return;
+function signInSucceed(result) {
+    if (result.credential) {
+        googleAccountToken = result.credential.accessToken;
+        user = result.user;
+
+        $("#photo").attr("src", user.photoURL);
+        $("#displayName").html(user.displayName);
+        $("#email").html(user.email);
+        $("#refreshToken").html(user.refreshToken);
+        $("#uid").html(user.uid);
+
+        $("#authorized").show();
+        $("#signIn").hide();
     }
+}
+
+function signInError(error) {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    var email = error.email;
+    var credential = error.credential;
+
+    var errmsg = errorCode + " " + errorMessage;
+
+    if(typeof(email) != 'undefined') {
+        errmsg += "<br />";
+        errmsg += "Cannot sign in with your google account: " + email;
+    }
+
+    if(typeof(credential) != 'undefined') {
+        errmsg += "<br />";
+        errmsg += credential;
+    }
+
+    lastWork = "signIn";
+    $("#error #errmsg").html(errmsg);
+    $("#error").show();
+    $("#signIn").hide();
+    return;
+}
+
+function signInWithRedirect() {
+    var provider = new firebase.auth.GoogleAuthProvider();
     
-    firebase.auth().signOut().then(function() {
-        location.reload();
-    }, function(e) {
-        lastWork = "authorized";
-        $("#error #errmsg").html(e.message)
-        $("#error").show();
-        $("#authorized").hide();
-    });
+    firebase.auth().signInWithRedirect(provider);
 }
 
 function back() {
